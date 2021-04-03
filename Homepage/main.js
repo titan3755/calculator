27dcc71f9ff2d8b5f3c9a65dbf76ef2a9aeef499
Titan <68808479+titan3755@users.noreply.github.com>
2021-04-03 21:47:13 +0600

    add

---
+++ Homepage/main.js
@@ -0,0 +1,9 @@
+let var1 = 'Wassap\nNigga!\tAHO VATIJA AHO\f\f\f\f\f\f\f'
+console.log(var1)
+let newNumber = 60*50*10000/1200
+console.log(newNumber/79+55)
+
+console.log('---------------------------------------------------')
+let myVar = 'AHO VATIJA AHO! '
+myVar += 'Taratari pant khulo!'
+console.log(myVar)
