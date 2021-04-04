2d68371e486b72e3903ee0d3f371274fdb1a6886
Titan <68808479+titan3755@users.noreply.github.com>
2021-04-04 15:11:44 +0600

    updated files

--- Homepage/main.js
+++ Homepage/main.js
@@ -1,9 +1,2 @@
-let var1 = 'Wassap\nNigga!\tAHO VATIJA AHO\f\f\f\f\f\f\f'
-console.log(var1)
-let newNumber = 60*50*10000/1200
-console.log(newNumber/79+55)
-
-console.log('---------------------------------------------------')
-let myVar = 'AHO VATIJA AHO! '
-myVar += 'Taratari pant khulo!'
-console.log(myVar)
+let one = ['eee', 'eeer']
+console.log(one[1])
